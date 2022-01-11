b1d484a5237969e322735e6c411d8bf1d069db8d
Thomas Normant <thomas.normant@uha.fr>
2022-01-11 11:40:34 +0100

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point p = new Point(1.0,2.0);
+        Point p2 = new Point(10.0,12.0);
+        System.out.println(p.distance(p2));
+    }
+}
---
+++ src/com/company/Point.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import static java.lang.Math.*;
+
+public class Point {
+    private double _x;
+    private double _y;
+
+    public Point(double x, double y) {
+        this._x = x;
+        this._y = y;
+    }
+
+    public double get_x() {
+        return _x;
+    }
+
+    public void set_x(double x) {
+        this._x = x;
+    }
+
+    public double get_y() {
+        return _y;
+    }
+
+    public void set_y(double y) {
+        this._y = y;
+    }
+
+    public double distance(Point p){
+        return sqrt(pow(p.get_x()-this._x,2)+pow(p.get_y()-_y,2));
+    }
+}
